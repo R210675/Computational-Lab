9317686f2c8c28e7d0adbe4e7deb099f5134750f
R210675 <159224053+R210675@users.noreply.github.com>
2024-05-01 14:33:23 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -2,7 +2,7 @@ def det(matrix):
 	if len(matrix)!=2 or len(matrix[1])!=2:
 		raise ValueError("Input matrix must be a 2*2 matrix")
 	return matrix[0][0]*matrix[1][1]-matrix[0][1]
-matrix_2=[[3,4],[1,2]]
+matrix_2=[[1,2],[3,4]]
 result=det(matrix_2)
 print("Determinant of 2*2 matrix",result)
 
